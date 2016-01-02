3e363b4eee64ba5482418c999d61df47a0a69b57
iiceman40 <sascha.siemer@gmx.de>
2016-01-02 12:20:04 +0100

    added movie list

--- js/services.js
+++ js/services.js
@@ -12,6 +12,30 @@ app.factory('MoviesDataService', function ($http) {
 				});
 			}
 			return promiseMovies;
+		},
+		getMoviesFromHtml: function () {
+			if (!promiseMovies) {
+				promiseMovies = $http.get('top250.html').then(function (moviesResponse) {
+					$('#movie-info').append(moviesResponse.data);
+					var chart = $('#movie-info').find('table.chart');
+					var movies = [];
+					chart.find('tr').each(function(){
+						var thisMovieInfo = $(this).find('td.titleColumn');
+						var title = thisMovieInfo.find('> a').text();
+						var year = parseInt(thisMovieInfo.find('span.secondaryInfo').text().replace('(', '').replace(')', ''));
+						if(title && !isNaN(year)) {
+							movies.push({
+								title: title,
+								year: year,
+								genres: []
+							});
+						}
+					});
+					console.log(JSON.stringify(movies));
+					return movies;
+				});
+			}
+			return promiseMovies;
 		}
 	};
 
